ac600161260165cb745d63c71549701f8a204bcf
becauseoffyou <irhandy96@gmail.com>
2026-09-08 17:03:48 +0700

    change url api

--- src/app.js
+++ src/app.js
@@ -52,7 +52,7 @@ app.get("/api/test-db", async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9090;
 
 app.listen(PORT, () => {
     console.log(`KANCHA API running on port ${PORT}`);
